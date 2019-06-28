42408abef6999be2f2e7e6341b283f236c2de8fd
Geek-dia <51751100+Geek-dia@users.noreply.github.com>
2019-06-28 11:54:23 +0000

    Add files via upload

---
+++ Website/account.js
@@ -0,0 +1,28 @@
+
+function New(){
+    var firstname = document.getElementById("first").value;
+    var surname = document.getElementById("sur").value;
+    var eml = document.getElementById("mail").value;
+    var passwd = document.getElementById("pass").value;
+    var modeofpayment = document.getElementById("pay").value;
+
+    var 
+    
+   
+    if (eml == Email && password.value == Password){
+
+        window.alert("Thanks for logging in");
+    }
+    else if()
+    {
+        window.alert("Wrong Inputs please try again");
+    }
+    else if()
+    { 
+        (email.value == "" && password.value == "");
+
+        window.alert("Invalid");
+
+        
+    
+    }
